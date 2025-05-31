514cd0252fb278aa3679e563d2f7dc9f54cf29f3
atefElhamsa <atefelhamsa79@gmail.com>
2025-06-01 01:54:34 +0300

    container type done

--- lib/features/home/presentation/views/widgets/home_body.dart
+++ lib/features/home/presentation/views/widgets/home_body.dart
@@ -1,10 +1,19 @@
+import 'package:bmi_app/features/home/presentation/views/widgets/list_view_container_type.dart';
 import 'package:flutter/material.dart';
 
-class HomeBody extends StatelessWidget {
+class HomeBody extends StatefulWidget {
   const HomeBody({super.key});
 
   @override
+  State<HomeBody> createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<HomeBody> {
+  @override
   Widget build(BuildContext context) {
-    return Column();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Column(children: [ListViewContainerType()]),
+    );
   }
 }
---
+++ lib/features/home/presentation/views/widgets/list_view_container_type.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../../data/models/container_type_item_widget_model.dart';
+import 'container_type_item_widget.dart';
+
+class ListViewContainerType extends StatefulWidget {
+  const ListViewContainerType({super.key});
+
+  @override
+  State<ListViewContainerType> createState() => _ListViewContainerTypeState();
+}
+
+class _ListViewContainerTypeState extends State<ListViewContainerType> {
+  int activeIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 160,
+      width: double.infinity,
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        separatorBuilder: (context, index) {
+          return const SizedBox(width: 50);
+        },
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              if (activeIndex != index) {
+                setState(() {
+                  activeIndex = index;
+                });
+              }
+            },
+            child: ContainerTypeItemWidget(
+              containerTypeItemWidgetModel: ContainerTypeItemWidgetModel(
+                text: texts[index],
+                iconData: icons[index],
+                isSelected: activeIndex == index,
+              ),
+            ),
+          );
+        },
+        itemCount: texts.length,
+      ),
+    );
+  }
+}
