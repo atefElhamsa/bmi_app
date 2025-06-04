4308917d84657c74f66a7c3e270e3333e1584c1a
atefElhamsa <atefelhamsa79@gmail.com>
2025-06-04 14:55:04 +0300

    application done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:bmi_app/features/home/data/repo/logic_provider.dart';
-import 'package:bmi_app/features/home/presentation/views/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
+
+import 'my_app.dart';
 
 void main() {
   runApp(
@@ -11,23 +11,4 @@ void main() {
       child: const MyApp(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 1006),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return const MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: HomeScreen(),
-        );
-      },
-    );
-  }
 }
---
+++ lib/my_app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'features/home/presentation/views/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 1006),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: HomeScreen(),
+        );
+      },
+    );
+  }
+}
